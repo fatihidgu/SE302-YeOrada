bf4872541c3f19c534b281d26d28aafba2f31ea7
fatihidgu <fatihidgu@gmail.com>
2020-04-16 21:57:44 +0300

    test

--- YeOrada/YeOradaApp/tests.py
+++ YeOrada/YeOradaApp/tests.py
@@ -2,6 +2,11 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
+from django.urls import reverse
+from YeOradaApp.models import*
+import unittest
+import os
+from django.test import TestCase
 
 
 class UsersManagersTests(TestCase):
@@ -42,3 +47,30 @@ class UsersManagersTests(TestCase):
         with self.assertRaises(ValueError):
             User.objects.create_superuser(
                 email='super@user.com', password='foo', is_superuser=False)
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/home/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_like_comment(self):
+        User = get_user_model()
+
+    def test_client_profile(self):
+        User = get_user_model()
+
+    def test_settings(self):
+        User = get_user_model()
+        user = User.objects.create_user(email='normal@user.com', password='foo')
+        self.assertEqual(user.email, 'normal@user.com')
+
+    def test_my_profile(self):
+        User = get_user_model()
+
+    def test_details(self):
+        response = self.client.get('/customer/details/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_index(self):
+        response = self.client.get('/customer/index/')
+        self.assertEqual(response.status_code, 200)
+
