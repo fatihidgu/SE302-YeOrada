0c3c4223aecaba3542723383d363e09858c8d11c
ahmetterlikci <ahmetcan.22@hotmail.com>
2020-05-20 23:56:51 +0300

    Mail service is added, new default comment png, requirements.txt is uptated, a new html for decline review email

--- YeOrada/YeOrada/settings.py
+++ YeOrada/YeOrada/settings.py
@@ -119,6 +119,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = "noreply.yeorada@gmail.com"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = "oradaye123*"
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "YeOradaApp", "static"),
 )
